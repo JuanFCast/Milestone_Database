3d6e9ba2d46b06de54f906a89958070dfab4a2f1
JuanFCastillo <1006071586@u.icesi.edu.co>
2022-04-30 23:46:14 -0500

    Corrections

--- src/model/Database.java
+++ src/model/Database.java
@@ -2,39 +2,47 @@ package model;
 
 import java.util.Random;
 
+import util.AVL;
+
 public class Database {
-	
+
 	private AVL<Person> perID;
 	private AVL<Person> perName;
 	private AVL<Person> perSurName;
 	private AVL<Person> perFullName;
-	private String estatura, edad;
-    
-    public Database(){
-        
-    }
+	public static String estatura;
+	private String edad;
+	private double cant = 100; 
 
-    public void generateEstatura(){
-        RandomGenerator e = new RandomGenerator(100, 200);
-        estatura = (e.generateInt() + " cm");
-        
-  
-    	
-    }
-    public void generateAge(){
-    	RandomGenerator a = new RandomGenerator(0, 14);
-    	RandomGenerator b = new RandomGenerator(15, 24);
-    	RandomGenerator c = new RandomGenerator(25, 54);
-    	RandomGenerator d = new RandomGenerator(55, 64);
-    	RandomGenerator e = new RandomGenerator(64, 100);
-    	
-    	if (a == ) {
-			
-		}
-    	
-        edad = (a.generateInt() + " edad");
-    	
-    	
-    }
+	public Database(){
+
+	}
+
+	public void generateEstatura(){
+		RandomGenerator e = new RandomGenerator(100, 200);
+		estatura = (e.generateInt() + " cm");
+		System.out.println(estatura = (e.generateInt() + " cm"));
+
+
+
+	}
+
+	public void generateAge(){
+		RandomGenerator a = new RandomGenerator(0, 14);
+		RandomGenerator b = new RandomGenerator(15, 24);
+		RandomGenerator c = new RandomGenerator(25, 54);
+		RandomGenerator d = new RandomGenerator(55, 64);
+		RandomGenerator e = new RandomGenerator(64, 100);
+		
+		
+		edad = (a.generateInt() + " años");
+		
+		
+		
+	}
+
+
+
 
 }
+
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,10 +2,14 @@ package ui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.Database;
+import model.Gender;
+import model.Person;
 
 public class Main extends Application{
 	
 	private DatabaseGUI appController;
+	private Database databaseController = new Database();
 
     public static void main(String []team) {
         launch(team);
@@ -16,6 +20,10 @@ public class Main extends Application{
 		appController = new DatabaseGUI();
 		appController.setMainStage(primaryStage);
 		appController.mainMenu();
+		System.out.println(new Person("susana", "morales", Gender.female, "4/4/4", "120", "Peruana").id());
+		databaseController.generateEstatura();
+		
+		
 	}
 
 }
