cd06c0d019e522ace0b5cb43203b6a5b02902bd6
JuanFCastillo <1006071586@u.icesi.edu.co>
2022-05-07 16:50:09 -0500

    Sets ChoiceBox

---
+++ src/model/SEARCH_TYPE.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum SEARCH_TYPE {
+	ID, Name, LastName, Fullname;
+}
+
+//Porfa, seetenle al choice box las palabras (ID, Name, LastName,  Full Name)
--- src/ui/SearchMenuGUI.java
+++ src/ui/SearchMenuGUI.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.Pane;
+import model.SEARCH_TYPE;
 
 public class SearchMenuGUI {
 
 	@FXML
 	private Pane mainPane;
+	
+	@FXML
+	private ChoiceBox<SEARCH_TYPE> typeSearch;
 	
 	public SearchMenuGUI(Pane p) throws IOException {
 		mainPane = p;
@@ -21,6 +26,7 @@ public class SearchMenuGUI {
 		fxmlloader.setController(this);
     	Parent log = fxmlloader.load();
     	mainPane.getChildren().setAll(log);
+    	typeSearch.getItems().addAll(SEARCH_TYPE.ID, SEARCH_TYPE.Name, SEARCH_TYPE.LastName, SEARCH_TYPE.Fullname);
 	}
 	
 	private void searching() {
